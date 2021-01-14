0df9f1e91de1b6bdd7877f31f2441a03ac912871
LogCreative <longlaoda123456@qq.com>
2021-01-14 16:48:30 +0800

    Script update

--- Abusolute2Relative.ps1
+++ Abusolute2Relative.ps1
@@ -10,8 +10,10 @@
 # For every batch file -Recurse
 Get-ChildItem . '*.bat' -Force -Recurse | ForEach-Object -Process {
 
-    (Get-Content $_)|
-        ForEach-Object { $_ -replace 'E:\801CS','.'} |      # Change Abusolute Position
-        Out-File $_ -Force                                  # Output file
+    $file = [io.Path]::Combine($_.DirectoryName, $_.FullName)
+
+    (Get-Content $file)|
+        ForEach-Object { $_ -replace 'E:\\801CS','.'} |      # Change Abusolute Position
+        Out-File $file -Encoding utf8 -Force               # TODO: Output file with Chinese Encoding? gbk?
 
 }
